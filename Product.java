ea496b8fa7cb12e442a4c0cf0b4fa9b278e1d808
Aaron Granado Vargas <aaron.granado@softtek.com>
2026-04-07 10:50:47 +0200

    Creacion del proyecto

---
+++ Product.java
@@ -0,0 +1,13 @@
+package helloworld.liquidaciondiversos;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Product {
+    private String code;
+    private String name;
+    List<ProductWarranty> productWarranties;
+}
